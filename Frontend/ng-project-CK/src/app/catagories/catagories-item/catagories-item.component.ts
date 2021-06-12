ac28226c67317c6bf1705abaa8ca441217c50921
troisang2404 <18130165@st.hcmuaf.edu.vn>
2021-06-12 19:52:02 +0700

    trang the loai, trang gioi thieu

---
+++ Frontend/ng-project-CK/src/app/catagories/catagories-item/catagories-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catagories-item',
+  templateUrl: './catagories-item.component.html',
+  styleUrls: ['./catagories-item.component.scss']
+})
+export class CatagoriesItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
