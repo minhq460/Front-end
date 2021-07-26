9d3b3d665fcc9724ceb117ea3ab347eceb4f3d0f
trung926 <18130259@st.hcmuaf.edu.vn>
2021-07-26 11:57:05 +0700

    Edit animation sidebar

--- Frontend/ng-project-CK/src/app/app.module.ts
+++ Frontend/ng-project-CK/src/app/app.module.ts
@@ -39,7 +39,6 @@ import { ScrolltopComponent } from './scrolltop/scrolltop.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StickyNavModule } from 'ng2-sticky-nav';
 import { ArticleContentComponent } from './single-post/article-content/article-content.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -82,7 +81,6 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     StickyNavModule,
     FormsModule,
     ReactiveFormsModule,
-    Ng2SearchPipeModule
   ],
   providers: [NewsService, NavService],
   bootstrap: [AppComponent],
